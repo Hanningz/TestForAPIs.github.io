30ac404840ec51557e9b8844d6cbc572ca191dd7
Hanningz <xiongqionghai@gmail.com>
2021-09-09 15:18:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 // Instantiating new EasyHTTP class
 const http = new EasyHTTP;
 // User Data
-var data = {
-id:2,
-value:"testdata"
-};
+var data = {"id":2,"value":"test for worng now"};
 
 // Update Post
 http.put(
-'http://localhost:8088/api/clients/wks-96445-mac.ad.tuni.fi/30601/',
+'http://localhost:8088/api/clients/wks-96445-mac.ad.tuni.fi/30602/0/2',
 	data)
 
 // Resolving promise for response data
